735ffce38200ccb5c435bfafbf3ffc3aa81de213
cortezbel20 <cortezbel20@gmail.com>
2025-12-15 16:51:09 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using TempoPrevisao_PP.ViewModels;
+
+namespace TempoPrevisao_PP
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new WeatherViewModel();
+        }
+
+       
+    }
+
+}
